10be4e8a3f65f807eadc0738fcf4be9d62f29673
Richard Hu <rh1521@ic.ac.uk>
2022-03-30 22:53:13 +0100

    Modify boot.cmd to load initramfs

--- board/licheepi/boot.cmd
+++ board/licheepi/boot.cmd
@@ -1,4 +1,5 @@
-setenv bootargs console=ttyS0,115200 panic=5 console=tty0 rootwait root=/dev/mmcblk0p2 earlyprintk rw
+setenv bootargs console=ttyS0,115200 panic=5 console=tty0 rootwait initrd=0x45000000 root=/dev/mmcblk0p2 earlyprintk rw
 load mmc 0:1 0x41000000 zImage
-load mmc 0:1 0x41800000 sun8i-v3s-licheepi-zero-dock.dtb
-bootz 0x41000000 - 0x41800000
+load mmc 0:1 0x43000000 sun8i-v3s-licheepi-zero-dock.dtb
+load mmc 0:1 0x45000000 rootfs.cpio.uboot
+bootz 0x41000000 0x45000000 0x43000000
