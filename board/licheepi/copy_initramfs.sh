50432e98c9a28faf02ae96818e1b452fce537b93
Richard Hu <rh1521@ic.ac.uk>
2022-03-30 22:53:58 +0100

    Modify copy_initramfs.sh to also copy u-boot image of initramfs.cpio.gz

--- board/licheepi/copy_initramfs.sh
+++ board/licheepi/copy_initramfs.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-echo "Copying initramfs image to board/licheepi"
+echo "Copying initramfs images to board/licheepi"
 cp output/images/rootfs.cpio.gz board/licheepi/initramfs.cpio.gz
+cp output/images/rootfs.cpio.uboot board/licheepi/initramfs.cpio.uboot
